21e5dab50b162f802deb00ff074bbb99d6c88947
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-25 21:42:34 +0500

    Navbar Issue Solve

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,17 +10,16 @@ const Hero = () => {
         <div className="hero-container">
           <img src={heroimage} alt="heroimage" className='hero-image' />
           <div className="overlay">
+            <nav>
+              <div className="links">
+
+              </div>
             <div className="logo">
               <img src={logos} alt="logos" className='logos' />
             </div>
-            <nav id='navbar'>
-              <div className="navbar1">
-                <div className="list1"><a href="#">About Us</a></div>
-                <div className="list2"><a href="#">Offer</a></div>
-                <div className="list3"><a href="#">facilities</a></div>
-                <div className="list4"><a href="#">Membership</a></div>
-              </div>
-              <div className="navbar2"></div>
+            <div className="lastlinks">
+              
+            </div>
             </nav>
           </div>
         </div>
