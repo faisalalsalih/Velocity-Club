edc9f57152adf4079540927648f1551e5ef29a9c
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-26 14:35:21 +0500

    Blur Button Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import heroimage from "../assets/Hero.jpg"
 import logos from "../assets/trophy.png"
+import menu from "../assets/menu.png"
+import search from "../assets/search.png"
 
 
 const Hero = () => {
@@ -12,13 +14,28 @@ const Hero = () => {
           <div className="overlay">
             <nav>
               <div className="links">
-
+                <div className="link1"><a href="#">About us</a></div>
+                <div className="link2"><a href="#">Offer</a></div>
+                <div className="link3"><a href="#">facilities</a></div>
+                <div className="link4"><a href="#">Membership</a></div>
               </div>
             <div className="logo">
               <img src={logos} alt="logos" className='logos' />
             </div>
             <div className="lastlinks">
-              
+              <div className="foot">
+                <div className="search">
+                  <input type="text" placeholder='Search here...' />
+                  <div className="circle1">
+                    <img src={search} alt="search" className='searcho' />
+                  </div>
+                </div>
+                <div className="book">
+                  <h3>Book Now</h3>
+                  <div className="circle2"></div>
+                </div>
+                <div className="hamburger"><img src={menu} alt="menu-icon" className='menu' /></div>
+              </div>
             </div>
             </nav>
           </div>
