c3348a460c9622dbe5374229595200b4b0b152ca
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-27 12:08:45 +0500

    big-text completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,14 +46,48 @@ const Hero = () => {
             </div>
             </nav>
             {/* NAVBAR ENDS HERE */}
+            {/* LEFT DIV START FROM HERE */}
             <div className="left">
               <div className="capsule">
                 <div className="persona"><img src={person1} alt="person1" className='person1' /></div>
                 <div className="persona"><img src={person2} alt="person2" className='person2' /></div>
                 <div className="persona"><img src={person3} alt="person3" className='person3' /></div>
               </div>
-              <div className="paragraph"></div>
+              <div className="paragraph"><p>
+                we are delivering a<br />high quality experience in a<br />welcoming and supporting<br />atmosphere</p>
+              </div>
+              <div className="paragraph2"><p>All in one-sport<br />facilities center</p></div>
+              <div className="button">
+                <h4>Join With us</h4>
+                <div className="circle1">
+                  <img src={arrow} alt="arrow" className='arrow' />
+                </div>
+              </div>
             </div>
+
+            {/* The End of the left section */}
+
+            {/* Social Media  */}
+            <div className="social-media">
+                <button className='social'>Instagram</button>
+                <button className='social-1'>Twitter</button>
+                <button className='social'>Facebook</button>
+            </div>
+
+            <div className="flex">
+            <div className="big-text">
+              <h1>Refresh, Relax, and Rediscover<br />Life's Simple Pleasure</h1>
+              <p>Rediscover the beauty in life simplest, Pleasures and let every moments<br />here remind you of the joy of acceleration.</p>
+            </div>
+
+            <div className="short-text">
+
+            </div>
+
+            </div>
+
+
+
           </div>
         </div>
       </section>
