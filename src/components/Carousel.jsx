7bbc669be05ab83ac6025fc99238a87ae28a24d1
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-28 11:54:56 +0500

    Button Added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,7 +5,13 @@ const Carousel = () => {
     <>
       <section id='carousel'>
         <div className="carousel-container">
-            
+            <div className="carousel-1">
+                <div className="shatingai">
+                    <a href="#center">Sports Center</a>
+                </div>
+            </div>
+            <div className="carousel-2"></div>
+            <div className="carousel-3"></div>
         </div>
       </section>
     </>
