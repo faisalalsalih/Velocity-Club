535c39a9490c4175cce24617351c5e9f93b9a202
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-28 12:05:38 +0500

    Carousel 1 completed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import arrow from "../assets/right-up.png"
 
 const Carousel = () => {
   return (
@@ -8,6 +9,13 @@ const Carousel = () => {
             <div className="carousel-1">
                 <div className="shatingai">
                     <a href="#center">Sports Center</a>
+                </div>
+
+                <h2>Welcome to Velocity Club,<br />where we inspire atheletes<br />and fitness enthusiasts to<br />reach new heights.</h2>
+
+                <div className="gato">
+                    <h3>Get In Touch</h3>
+                    <div className="circle1"><img src={arrow} alt="arrow" className='arrow' /></div>
                 </div>
             </div>
             <div className="carousel-2"></div>
