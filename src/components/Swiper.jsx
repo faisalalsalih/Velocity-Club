64effdfd98a1d300737785b1a124b5ae623e0351
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-11-10 20:43:35 +0500

    its time for the last move

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import swipe from "../assets/swipe.jpg"
 
 const Swiper = () => {
   return (
     <>
       <section id='swiper'>
-        
+        <div className="heading">
+          <h1>We aim to empower atheletes<br />by instilling confidence</h1>          
+        </div>
+
+        <div className="swiper-control">
+          <div className="swipe1">
+            <img src={swipe} alt="swipe-img1" className='swipe-img' />
+            <div className="parda">
+              <div className="parda1">
+                <div className="circlo"></div>
+              </div>
+              <div className="parda2"></div>
+            </div>
+          </div>
+          <div className="swipe2"></div>
+        </div>
       </section>
     </>
   )
