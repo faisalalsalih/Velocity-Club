7020887c72f6539b8f5cbf49bb189271df791438
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-11-11 15:15:46 +0500

    Add Badges

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import swipe from "../assets/swipe.jpg"
+import badge from "../assets/badge.png"
+import star1 from "../assets/star1.jpg"
+import star2 from "../assets/star2.jpg"
+import star3 from "../assets/star3.jpg"
+import plus from "../assets/plus.png"
 
 const Swiper = () => {
   return (
@@ -14,8 +19,19 @@ const Swiper = () => {
             <img src={swipe} alt="swipe-img1" className='swipe-img' />
             <div className="parda">
               <div className="parda1">
-                <div className="circlo"></div>
-                <div className="widebar"></div>
+                <div className="circlo">
+                  <img src={badge} alt="badge" className='badge' />
+                </div>
+                <div className="widebar">
+                  <div className="widebar1">
+                    <div className="image-roller1"><img src={star1} alt="star1" className='star1' /></div>
+                    <div className="image-roller2"><img src={star2} alt="star2" className='star2' /></div>
+                    <div className="image-roller3"><img src={star3} alt="star3" className='star3' /></div>
+                  </div>
+                  <div className="widebar2">
+                    <img src={plus} alt="plus" className='suss' />
+                  </div>
+                </div>
               </div>
               <div className="parda2"></div>
             </div>
