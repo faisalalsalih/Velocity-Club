e96934ef6d18dc47b431ef8c07a5476f74b1f7ee
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-11-15 14:49:32 +0500

    Grabbing Functionality Added for Desktop Again For Another Component

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { RiArrowRightUpLine } from "react-icons/ri";
 import { FaUserGroup } from "react-icons/fa6";
 
 
 
 const Team = () => {
+
+  const teamref = useRef(null);
+
+  let isDraging = false;
+
+  let StartX;
+
+  let scrollLeft;
+
+  useEffect(() => {
+    const slider = teamref.current;
+
+    const mouseDownHandler = (e) => {
+      isDraging = true;
+      slider.classList.add('drag');
+      StartX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    }
+
+    const mouseMoveHandler = (e) => {
+      if (!isDraging) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - StartX) * 2;
+      slider.scrollLeft = scrollLeft - walk;
+    }
+    const mouseLeaveHandler = () => {
+      isDraging = false;
+      slider.classList.remove('drag');
+    }
+
+    const handlemouseUp = () => {
+      isDraging = false;
+      slider.classList.remove('drag');
+    }
+
+    slider.addEventListener('mousedown', mouseDownHandler);
+    slider.addEventListener('mouseleave', mouseLeaveHandler);
+    slider.addEventListener('mouseup', handlemouseUp);
+    slider.addEventListener('mousemove', mouseMoveHandler);
+
+    return () => {
+      slider.removeEventListener('mousedown', mouseDownHandler);
+      slider.removeEventListener('mouseleave', mouseLeaveHandler);
+      slider.removeEventListener('mouseup', handlemouseUp);
+      slider.removeEventListener('mousemove', mouseMoveHandler);
+    }
+  }, [])
   return (
     <>
       <section id='team'>
@@ -26,7 +74,7 @@ const Team = () => {
                 </div>
             </div>
         </div>
-        <div className="team2">
+        <div className="team2" ref={teamref}>
           <div className="pager1"></div>
           <div className="pager2"></div>
           <div className="pager3"></div>
