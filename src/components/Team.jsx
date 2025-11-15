d642dd24085f7bf22a20c947944a7ae0c9ec8964
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-11-15 18:00:37 +0500

    Four Cards Completed Out Of 6

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,6 +5,9 @@ import tennis1 from "../assets/tennisc1.jpg"
 import tennis2 from "../assets/tennisc2.jpg"
 import badminton from "../assets/badminton.jpg"
 import basketball from "../assets/basketball.jpg"
+import { LuMessageCircleMore } from "react-icons/lu";
+import { FaRegEnvelope } from "react-icons/fa";
+
 
 
 const Team = () => {
@@ -86,24 +89,94 @@ const Team = () => {
             <img src={tennis1} alt="pagers-pic" className='pagers-pic' />
             <div className="pager-overlay">
               <div className="dev-ops">
-                
+                <div className="devops-icons">
+                  <div className="bum-bum">
+                    <FaRegEnvelope className='enveloped'/>
+                  </div>
+                  <div className="bum-bum">
+                    <LuMessageCircleMore className='message'/>
+                  </div>
+                </div>
+              </div>
+
+              <div className="dev-ops2">
+                <div className="blur-dev">
+                  <h2>Alex Josh</h2>
+                  <h6>Tennis Coach</h6>
+                </div>
               </div>
             </div>
           </div>
           <div className="pager2">
             <img src={tennis2} alt="pagers-pic" className='pagers-pic' />
-            <div className="pager-overlay"></div>
+            <div className="pager-overlay">
+              <div className="dev-ops">
+                <div className="devops-icons">
+                  <div className="bum-bum">
+                    <FaRegEnvelope className='enveloped'/>
+                  </div>
+                  <div className="bum-bum">
+                    <LuMessageCircleMore className='message'/>
+                  </div>
+                </div>
+              </div>
+
+              <div className="dev-ops2">
+                <div className="blur-dev">
+                  <h2>Jacob Jones</h2>
+                  <h6>Tennis Coach</h6>
+                </div>
+              </div>
+            </div>
 
           </div>
-          <div className="pager3"></div>
+          <div className="pager3">
+            
+          </div>
           <div className="pager1">
             <img src={badminton} alt="pagers-pic" className='pagers-pic' />
-            <div className="pager-overlay"></div>
+            <div className="pager-overlay">
+              <div className="dev-ops">
+                <div className="devops-icons">
+                  <div className="bum-bum">
+                    <FaRegEnvelope className='enveloped'/>
+                  </div>
+                  <div className="bum-bum">
+                    <LuMessageCircleMore className='message'/>
+                  </div>
+                </div>
+              </div>
+
+              <div className="dev-ops2">
+                <div className="blur-dev">
+                  <h2>Jack Tom</h2>
+                  <h6>Badminton Coach</h6>
+                </div>
+              </div>
+            </div>
 
           </div>
           <div className="pager2">
             <img src={basketball} alt="pagers-pic" className='pagers-pic' />
-            <div className="pager-overlay"></div>
+            <div className="pager-overlay">
+              <div className="dev-ops">
+                <div className="devops-icons">
+                  <div className="bum-bum">
+                    <FaRegEnvelope className='enveloped'/>
+                  </div>
+                  <div className="bum-bum">
+                    <LuMessageCircleMore className='message'/>
+                  </div>
+                </div>
+              </div>
+
+              <div className="dev-ops2">
+                <div className="blur-dev">
+                  <h2>Rabada Sr</h2>
+                  <h6>Basketball Coach</h6>
+                </div>
+              </div>
+            </div>
 
           </div>
           <div className="pager3">
